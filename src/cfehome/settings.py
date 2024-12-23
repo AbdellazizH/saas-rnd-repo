f39ea4c4229d04b8835c1d72945634346a0dae1f
Aziz21120 <doukali_fr@live.fr>
2024-12-23 20:36:35 +0100

    add static directory and configure settings

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -125,6 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / "assets",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
